a2f936ead040ba819093d20babdf884f652a1ca6
Arthur Shirin <artshirin2009@gmail.com>
2018-10-30 11:15:54 +0200

    vcsdc

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 
-
+var vsdvds;
 
 var indexRouter = require('./routes/index');
 
